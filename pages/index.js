f224992bd638abd70cb351b98cc9675feac86807
Joe Carre <joe.carre@rfa.co.uk>
2018-06-01 11:54:56 +0100

    Finished tutorial
    Instructions to use:
    
    -cd into repo root
    -yarn run client
    -yarn run server

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import axios from 'axios'
+import Pusher from 'pusher-js'
+import Chart from '../components/Chart'
+import VoteButtons from '../components/Vote-buttons'
+
+var pusher = new Pusher('4ec675a3678a52dcf470', {
+  cluster: 'cluster',
+  encrypted: true
+})
+const channel = pusher.subscribe('NebuPoll')
 
 export default class Index extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: [0, 0, 0]
+    }
+  }
+
+  componentDidMount () {
+    this.receiveUpdateFromPusher()
+  }
+
+  receiveUpdateFromPusher () {
+    channel.bind('new-votes', data => {
+      this.setState({
+        data
+      })
+    })
+    console.log('app subscription to event successful')
+  }
+
+  handleVote (data) {
+    axios.post('http://localhost:8080/vote', data)
+    .then(res => {
+      console.log('received by server')
+    })
+    .catch(error => {
+      throw error
+    })
+  }
 
   render () {
     return (
       <div>
-        Hello world
+        <Chart data={this.state.data} />
+        <VoteButtons handleVote={this.handleVote.bind(this)} />
       </div>
     )
   }
